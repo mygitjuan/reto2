04c28ffffe749482ac8f3b4998785928b6b9a5d9
myGitJuan <jarellano.aguirre@gmail.com>
2024-01-01 13:30:02 +0100

    modificacion historia baja

---
+++ BananaWhatsApp/sql/mensajesParaBorrarCaso5Mensaje.sql
@@ -0,0 +1,28 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.4.1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Nov 25, 2023 at 12:46 PM
+-- Server version: 5.7.11
+-- PHP Version: 5.6.18
+
+
+
+--
+-- Database: `bananawhatsappdb`
+--
+
+--
+-- Dumping data for table `mensaje`
+--
+
+INSERT INTO `mensaje` (`id`, `cuerpo`, `fecha`, `from_user`, `to_user`) VALUES
+(26, '¿Has visto Star Trek?', '2024-01-01', 29, 30),
+(27, 'No nunca', '2024-01-01', 30, 29),
+(28, '¿Qué es?', '2024-01-01', 30, 29),
+(29, '¿Conoces Star Trek?', '2024-01-01', 31, 29);
+
+
+
+
---
+++ BananaWhatsApp/sql/mensajesParaBorrarCaso5Usuario.sql
@@ -0,0 +1,24 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.4.1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Nov 25, 2023 at 12:46 PM
+-- Server version: 5.7.11
+-- PHP Version: 5.6.18
+
+
+
+--
+-- Database: `bananawhatsappdb`
+--
+
+--
+-- Dumping data for table `usuario`
+--
+
+INSERT INTO `usuario` (`id`, `activo`, `alta`, `email`, `nombre`) VALUES
+(29, b'1', '2024-01-01', 'mariner@o.com', 'Mariner'),
+(30, b'1', '2024-01-01', 'boimler@b.com', 'Boimler'),
+(31, b'1', '2024-01-01', 'dvana@d.com', 'Dvana');
+
